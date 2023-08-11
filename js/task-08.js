c5a0ed18a3e8183cfd85c7009ded70b33ff75485
Max-IT-ZT <babuskomax@gmail.com>
2023-08-12 01:13:40 +0300

    lastCommit

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const formObject = Object.fromEntries(formData.entries());
+  if (formObject.email === "" || formObject.password === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    console.log(formObject);
+    loginForm.reset();
+  }
+}
