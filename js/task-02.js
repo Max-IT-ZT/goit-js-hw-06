547800f838b52899bf0cedbe766f9317d751b6fb
Max-IT-ZT <babuskomax@gmail.com>
2023-08-12 15:40:24 +0300

    fix

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,11 @@ const ingredients = [
   "Condiments",
 ];
 const list = document.querySelector("#ingredients");
-
+const fragment = document.createDocumentFragment();
 ingredients.forEach((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
-  list.append(li);
+  fragment.appendChild(li);
 });
+list.appendChild(fragment);
